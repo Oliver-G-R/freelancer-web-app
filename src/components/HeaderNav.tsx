658b1f3d215e89b174f7ffd202270f84e760a5d0
Oliver <oliver.kali.hub@gmail.com>
2024-07-09 23:36:46 -0600

    fix: bug al hacer scroll

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,7 +1,32 @@
+"use client";
 import Link from 'next/link';
+import { useEffect, useRef } from 'react';
 export const HeaderNav = () => {
+  const header = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+
+    const handleScroll = () => {
+       if (window.scrollY > 0) {
+        header.current?.classList.add('global-gradient')
+      } else {
+        header.current?.classList.remove('global-gradient')
+      }
+    }
+
+    handleScroll()
+    window.addEventListener('scroll', () => handleScroll())
+
+    return () => {
+      window.removeEventListener('scroll', () => handleScroll())
+    }
+
+
+
+
+
+  }, [])
   return (
-    <header className='fixed w-full '>
+    <header ref={header} className='transition fixed w-full z-[5]'>
       <nav className='container m-auto p-5 flex items-center justify-between'>
         <h2 className='font-semibold text-white text-2xl'>
           CodeCrafters
