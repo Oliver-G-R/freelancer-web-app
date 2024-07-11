3fe0c1272df1e3029890683a57543ada8bfa0f2a
Oliver_HUB <Oliver.kali.hub@gmail.com>
2024-07-11 22:17:21 +0000

    add: seccion de crear o iniciar cuenta en formulario

--- src/app/auth/_components/FormAuth.tsx
+++ src/app/auth/_components/FormAuth.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link"
 import { FormEvent } from "react"
 
-interface InputProps {
+interface FormAuthProps {
   legendTitle: string
   children : React.ReactNode[]
   btnTitle: string
+  type?: 'create' | 'login'
   handleSubmit: (e:FormEvent) => void
 }
 
-export const FormAuth = ({handleSubmit, children, legendTitle, btnTitle}:InputProps) => {
+export const FormAuth = ({handleSubmit, children, legendTitle, btnTitle, type = 'create'}:FormAuthProps) => {
   return (
      <form onSubmit={e => handleSubmit(e)}>
         <fieldset>
@@ -21,8 +23,40 @@ export const FormAuth = ({handleSubmit, children, legendTitle, btnTitle}:InputPr
           <button className="text-xl text-center bg-[--color-secondary] text-white rounded-md w-full p-2">
             {btnTitle}
           </button>
+
+          {
+            type === 'create' 
+              ? <SectionFormAuth 
+                  title="¿Ya tienes una cuenta"
+                  btnTitle="Iniciar sesión" 
+                  path="/auth/login"
+                />
+              : <SectionFormAuth 
+              title="¿Aún no tienes una cuenta?"
+              btnTitle="Registrate" 
+              path="/auth/register/type-account"
+            />
+          }
+
         </fieldset>
       </form>
 
+  )
+}
+
+interface SectionFormAuthProps{
+  title: string
+  btnTitle: string
+  path: string
+} 
+
+const SectionFormAuth = ({title, path, btnTitle}:SectionFormAuthProps) => {
+  return(
+    <div className="flex gap-4 border-t-[1.5px] mt-6 pt-4">
+        <span>{title}</span>
+        <Link className="text-blue-500" href={path}>
+          {btnTitle}
+        </Link>
+    </div>
   )
 }
