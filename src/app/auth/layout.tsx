f04d38c472f72d6855b4ab45f10c8702ddf7b20a
Oliver <oliver.kali.hub@gmail.com>
2024-07-21 16:55:35 -0600

    fix: form card

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -3,7 +3,7 @@ interface LayoutAuthProps{
 }
 const layoutAuth = ({children}:LayoutAuthProps) => {
   return (
-     <main className="global-container flex flex-col justify-center items-center h-[100vh]">
+     <main className="global-container flex flex-col justify-center pt-20 items-center ">
         <div className="rounded-xl shadow-lg bg-white px-10 py-5 w-[600px]   max-sm:w-full">
           <span className="font-semibold text-[--color-secondary] text-center text-3xl block">
             CodeCrafters
